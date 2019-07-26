dcc568d99735b0806bc6bc8fc579382bb958672e
yecarrillo <636224064947214727071github*@>
2019-07-26 10:08:35 -0500

    Nueva URL

--- public/front/app/assets/js/constants.js
+++ public/front/app/assets/js/constants.js
@@ -1,5 +1,5 @@
 (function () {
     angular.module('koboG.constants', [])
     //.constant('API_URL', 'http://apikobo:8888/api/')
-    .constant('API_URL', 'http://localhost/main/umaic/public/api/')
+    .constant('API_URL', 'https://salahumanitaria.co/mira/api/')
 })();
